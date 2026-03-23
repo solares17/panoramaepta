907a5a4ff42076eabbdd127fb3d5b26d2d33e392
solares17 <52794037+solares17@users.noreply.github.com>
2026-03-23 12:52:17 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,49 @@
+var viewer = pannellum.viewer('panorama', {
+
+"default": {
+    "firstScene": "room1",
+    "sceneFadeDuration": 1000,
+    "autoLoad": true
+},
+
+"scenes": {
+
+    "room1": {
+        "title": "Комната 1",
+        "type": "equirectangular",
+        "panorama": "room1.jpg",
+
+        "hotSpots": [
+            {
+                "pitch": 0,
+                "yaw": 90,
+                "type": "scene",
+                "text": "Перейти в комнату 2",
+                "sceneId": "room2"
+            }
+        ]
+    },
+
+    "room2": {
+        "title": "Комната 2",
+        "type": "equirectangular",
+        "panorama": "room2.jpg",
+
+        "hotSpots": [
+            {
+                "pitch": 0,
+                "yaw": -90,
+                "type": "scene",
+                "text": "Назад в комнату 1",
+                "sceneId": "room1"
+            }
+        ]
+    }
+
+}
+
+});
+
+function loadScene(scene){
+    viewer.loadScene(scene);
+}
