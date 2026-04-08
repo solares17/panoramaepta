cc6fac176383b767093858a86e6890a8c9a2ba7f
solares17 <52794037+solares17@users.noreply.github.com>
2026-04-09 00:28:33 +0500

    Update include.js

--- scripts/include.js
+++ scripts/include.js
@@ -1,9 +1,9 @@
 
-fetch("./pages/header.html")
+fetch("/pages/header.html")
     .then(resp => resp.text())
     .then(data => document.getElementById("include-header").innerHTML = data);
 
 // Подключение footer
-fetch("./pages/footer.html")
+fetch("/pages/footer.html")
     .then(resp => resp.text())
     .then(data => document.getElementById("include-footer").innerHTML = data);
