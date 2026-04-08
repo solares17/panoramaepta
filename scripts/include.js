ee42521043c73c839776bceb551dad54a75f661b
solares17 <52794037+solares17@users.noreply.github.com>
2026-04-09 00:50:27 +0500

    Update include.js

--- scripts/include.js
+++ scripts/include.js
@@ -1,8 +1,8 @@
-fetch("panoramaepta//pages/header.html")
+fetch("/panoramaepta/pages/header.html")
     .then(r => r.text())
     .then(html => document.getElementById("include-header").innerHTML = html);
 
 // ЗАГРУЗКА ФУТЕРА
-fetch("panoramaepta/pages/footer.html")
+fetch("/panoramaepta/pages/footer.html")
     .then(r => r.text())
     .then(html => document.getElementById("include-footer").innerHTML = html);
