4fb32f4f513d769ad691582f40944cefb94df1b6
solares17 <52794037+solares17@users.noreply.github.com>
2026-04-09 19:20:57 +0500

    Update cardsdata.js

--- scripts/cardsdata.js
+++ scripts/cardsdata.js
@@ -17,5 +17,68 @@ const equipment = [
             { name: "ЛР №1 — Генерация синуса", url: "labs/lab3.html" },
             { name: "ЛР №2 — Работа с меандром", url: "labs/lab4.html" }
         ]
+    },
+    {
+        img: "/panoramaepta/images/freq1.jpg",
+        title: "Исследование частотных детекторов",
+        desc: "Прибор для формирования периодических сигналов.",
+        labs: [
+            { name: "ЛР №1 — Генерация синуса", url: "labs/lab3.html" },
+            { name: "ЛР №2 — Работа с меандром", url: "labs/lab4.html" }
+        ]
+    },
+     {
+        img: "/panoramaepta/images/UOZ1.jpg",
+        title: "УОС-01. Преобразователь частоты",
+        desc: "Прибор для формирования периодических сигналов.",
+        labs: [
+            { name: "ЛР №1 — Генерация синуса", url: "labs/lab3.html" },
+            { name: "ЛР №2 — Работа с меандром", url: "labs/lab4.html" }
+        ]
+    },
+     {
+        img: "/panoramaepta/images/UOZ2.jpg",
+        title: "УОС-02. Амплитудный детектор",
+        desc: "Прибор для формирования периодических сигналов.",
+        labs: [
+            { name: "ЛР №1 — Генерация синуса", url: "labs/lab3.html" },
+            { name: "ЛР №2 — Работа с меандром", url: "labs/lab4.html" }
+        ]
+    },
+     {
+        img: "/panoramaepta/images/UOZ3.jpg",
+        title: "УОС-03. Частотный детектор",
+        desc: "Прибор для формирования периодических сигналов.",
+        labs: [
+            { name: "ЛР №1 — Генерация синуса", url: "labs/lab3.html" },
+            { name: "ЛР №2 — Работа с меандром", url: "labs/lab4.html" }
+        ]
+    },
+     {
+        img: "/panoramaepta/images/UOZ4.jpg",
+        title: "УОС-04. Входная цепь",
+        desc: "Прибор для формирования периодических сигналов.",
+        labs: [
+            { name: "ЛР №1 — Генерация синуса", url: "labs/lab3.html" },
+            { name: "ЛР №2 — Работа с меандром", url: "labs/lab4.html" }
+        ]
+    },
+    {
+        img: "/panoramaepta/images/UOZ5.jpg",
+        title: "УОС-05. Фазовая автоподстройка частоты",
+        desc: "Прибор для формирования периодических сигналов.",
+        labs: [
+            { name: "ЛР №1 — Генерация синуса", url: "labs/lab3.html" },
+            { name: "ЛР №2 — Работа с меандром", url: "labs/lab4.html" }
+        ]
+    },
+    {
+        img: "/panoramaepta/images/UOZ6.jpg",
+        title: "УОС-06. Автоматическая регулировка усиления",
+        desc: "Прибор для формирования периодических сигналов.",
+        labs: [
+            { name: "ЛР №1 — Генерация синуса", url: "labs/lab3.html" },
+            { name: "ЛР №2 — Работа с меандром", url: "labs/lab4.html" }
+        ]
     }
 ];
