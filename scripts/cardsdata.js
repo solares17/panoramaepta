443279fc35a93136b4e0c3e693c52304676baa5e
solares17 <52794037+solares17@users.noreply.github.com>
2026-04-09 18:43:09 +0500

    Create cardsdata.js

---
+++ scripts/cardsdata.js
@@ -0,0 +1,21 @@
+const equipment = [
+    {
+        img: "img/oscilloscope.png",
+        title: "Осциллограф",
+        desc: "Устройство для измерения формы сигналов.",
+        labs: [
+            { name: "ЛР №1 — Измерение амплитуды", url: "labs/lab1.html" },
+            { name: "ЛР №2 — Частотный анализ", url: "labs/lab2.html" }
+        ]
+    },
+  
+    {
+        img: "img/generator.png",
+        title: "Генератор сигналов",
+        desc: "Прибор для формирования периодических сигналов.",
+        labs: [
+            { name: "ЛР №3 — Генерация синуса", url: "labs/lab3.html" },
+            { name: "ЛР №4 — Работа с меандром", url: "labs/lab4.html" }
+        ]
+    }
+];
