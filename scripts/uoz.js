25b80440dbb5887c0b18847e7c02af8f6d085d53
solares17 <52794037+solares17@users.noreply.github.com>
2026-04-29 19:29:21 +0500

    Update uoz.js

--- scripts/uoz.js
+++ scripts/uoz.js
@@ -14,7 +14,7 @@ function update() {
   const f_c = +fc.value;
   const f_g = +fg.value;
   const U_c = +uc.value;
-
+  const detector = getDetector();
   const f_if1 = f_g - f_c;
   const f_if2 = f_c - f_g;
 
@@ -29,9 +29,13 @@ function update() {
   render(modeSelect.value, f_if1, f_if2, U_if, U_out);
 }
 
+function getDetector() {
+  return document.querySelector('input[name="detector"]:checked').value;
+}
+
 function render(mode, f_if1, f_if2, U_if, U_out) {
   let html = "";
-
+  
   if (mode == 1) {
     html += `fп.ч = fг – fс = ${f_if1} кГц<br>`;
     html += `Uсигнала выходного = ${U_out} В`;
@@ -56,7 +60,25 @@ function render(mode, f_if1, f_if2, U_if, U_out) {
     html += `Uп.ч = ${U_if} В`;
   }
 
+  let U_if, U_out;
+
+if (detector == 1) {
+  U_if = (U_c * 0.8).toFixed(3);
+  U_out = (U_c * 0.6).toFixed(3);
+}
+
+if (detector == 2) {
+  U_if = (U_c * 0.5).toFixed(3);
+  U_out = (U_c * 0.4).toFixed(3);
+}
+
+if (detector == 3) {
+  U_if = (U_c * 0.3).toFixed(3);
+  U_out = (U_c * 0.2).toFixed(3);
+}
+
   screen.innerHTML = html;
+  html += `Детектор: ${detector}<br><br>`;
 }
 
 fc.oninput = update;
