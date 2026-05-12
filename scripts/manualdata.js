0c124c6e826810d5d348e7823ba0f371f3d6ad0e
solares17 <52794037+solares17@users.noreply.github.com>
2026-05-13 01:49:46 +0500

    Update manualdata.js

--- scripts/manualdata.js
+++ scripts/manualdata.js
@@ -20,3 +20,36 @@ const manuals = [
         downloadUrl: "/panoramaepta/data/uos3.docx" // Укажи правильный путь к файлу
     },
 ];
+
+const manualsList = document.querySelector(".manuals-list");
+
+if (manualsList) {
+    manuals.forEach(manual => {
+        const card = document.createElement("div");
+        card.classList.add("manual-card");
+
+        // Обрати внимание на тег <img> и класс .download-btn
+        card.innerHTML = `
+            <div class="card-header">
+                <img src="${manual.img}" class="equip-img" alt="${manual.title}">
+                <div class="equip-info">
+                    <h3>${manual.title}</h3>
+                </div>
+            </div>
+
+            <div class="card-details">
+                <p>${manual.text}</p>
+                <a href="${manual.downloadUrl}" download target="_blank" class="download-btn">
+                    Скачать PDF методичку
+                </a>
+            </div>
+        `;
+
+        card.addEventListener("click", function(event) {
+            if(event.target.tagName === 'A') return; 
+            card.classList.toggle("active");
+        });
+
+        manualsList.appendChild(card);
+    });
+}
